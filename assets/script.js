35bb629c5758e915e59696dba46d169f147b5c6f
EvanEbert <evan.ebert17@gmail.com>
2021-06-24 21:41:26 -0500

    added variables for window.prompts and character types (subject to deletion)

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
+var buttonInteract = document.querySelector("#generate");
+var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberPass = "0123456789";
+var symbols = "!@#$%^&*_-+=";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var windowReady = window.confirm("Ready?");
+  var windowLength = window.prompt("How many Characters? (8-128)");
+  var windowUpper = window.confirm("Want Uppercase letters in the password?");
+  var windowLower = window.confirm("Want Lowercase letters in the password?");
+  var windowNumbers = window.confirm("Numbers?");
+  var windowSpecial = window.confirm("Special Characters?");
+  
+    var password = {
+      generatePassword() {
+         {
+
+        }
+      }
+    };
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -12,4 +29,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+buttonInteract.addEventListener("click", writePassword);
