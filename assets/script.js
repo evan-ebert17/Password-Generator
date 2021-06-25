a0c796be1dd2d0c038cb6bb7d5ee1e59782cb7cc
EvanEbert <evan.ebert17@gmail.com>
2021-06-25 02:31:09 -0500

    added the password content variables

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,26 @@
 // Assignment Code
 var buttonInteract = document.querySelector("#generate");
-var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
-var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberPass = "0123456789";
-var symbols = "!@#$%^&*_-+=";
+// var windowReady = window.confirm("Ready?");
+// var windowLength = window.prompt("How many Characters? (8-128)", "8-128");
+// var windowUpper = window.confirm("Want Uppercase letters in the password?");
+// var windowLower = window.confirm("Want Lowercase letters in the password?");
+// var windowNumbers = window.confirm("Numbers?");
+// var windowSpecial = window.confirm("Special Characters?");
 
 // Write password to the #password input
 function writePassword() {
-  var windowReady = window.confirm("Ready?");
-  var windowLength = window.prompt("How many Characters? (8-128)");
-  var windowUpper = window.confirm("Want Uppercase letters in the password?");
-  var windowLower = window.confirm("Want Lowercase letters in the password?");
-  var windowNumbers = window.confirm("Numbers?");
-  var windowSpecial = window.confirm("Special Characters?");
-  
-    var password = {
-      generatePassword() {
-         {
-
-        }
-      }
-    };
-  
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 buttonInteract.addEventListener("click", writePassword);
+ 
+function generatePassword() {
+var charactersPass = "abcdefghijklmnopqrstuvABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbersPass = "0123456789";
+var numberOfCharMin = 8;
+var numberOfCharMax = 128;
+ }
